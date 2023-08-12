d597ad6459b80b7265c5105f939fe15f7eb921b3
plvufpt <140293925+plvufpt@users.noreply.github.com>
2023-08-12 14:36:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,10 +56,37 @@ st.subheader("Daily return")
 # về công thức gốc thì giá trị ret = (giá ngày hôm nay - giá ngày trước đó) - 1
 multiple_stocks_ret = multiple_stock.pct_change(1)
 st.write(multiple_stocks_ret)
-
-# Write Daily return
-
-
+st.subheader("Calculate Risk Portfolio")
+# Second, We need to do is to calculate risk portfolio so we need 2 thing:
+  #vector of weight Omega
+  #variance covariance matrix or sigma
+#vector of weight Omega
+# number_stock bằng 10 vì chia đều cho 10 mã cổ phiếu
+number_stock = 10
+weight = [1 / number_stock] * number_stock
+weight
+st.subheader("Variance Covariance Matrix")
+#variance covariance matrix or sigma
+# này là tính mối liên hệ giữa các cổ phiếu với nhau
+vcv_matrix = multiple_stocks_ret.cov()
+st.write(vcv_matrix)
+st.subheader("Variance Portfolio")
+#tính variance portfolio
+var_p = np.dot(np.transpose(weight), np.dot(vcv_matrix, weight))
+st.write("var_p")
+st.subheader("STD of Portfolio")
+#tính std của portfolio = căn bậc 2 của variance portfolio
+std_p = np.sqrt(var_p)
+st.write("std_p")
+st.subheader("STD Portfolio Annual")
+#tuy nhiên std_p hiện tại đang tính là daily và nếu mình muốn tính annual thì nhân nó cho căn bậc 2 của 250 trong đó 250 là số ngầy gd của 1 năm
+std_p_annual = std_p * np.sqrt(250)
+st.write("std_p_annual")
+# tuy nhiên mục đích chúng ta tạo ra danh mục này chính là compare các danh mục đầu tư rủi ro này với rủi ro cá nhân
+# để tính rủi ro từng danh mục ta lấy độ lệch chuẩn của 'multiple_stocks_ret' các cổ phiếu hàng ngày. Tuy nhiên chúng tôi muốn tính std của hàng năm vì thế tôi nhân cho căn bậc 2 của 250
+st.subheader("individual_risks")
+individual_risks = np.std(returns_df) * np.sqrt(250)
+st.write("individual_risks")
 
 
 
