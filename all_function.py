a4900132cd89ba29b22fabf55bf0b6bc464f7790
plvufpt <140293925+plvufpt@users.noreply.github.com>
2023-08-12 13:50:52 +0700

    Update all_function.py

--- all_function.py
+++ all_function.py
@@ -1,3 +1,5 @@
+import vnstock
+import pandas
 def get_stock(stock):
   stock = vnstock.stock_historical_data(stock,'2018-01-01', '2023-01-01')
   stock.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Vol', 'Ticker']
