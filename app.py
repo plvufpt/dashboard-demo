1d1e79f428d384a4adc36f3764a33dbfcc07b030
plvufpt <140293925+plvufpt@users.noreply.github.com>
2023-08-12 13:47:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ import investpy as ipy
 import streamlit as st
 import all_function
 
-def get_stock(stock):
-  stock = vnstock.stock_historical_data(stock,'2018-01-01', '2023-01-01')
-  stock.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Vol', 'Ticker']
-  stock['Date'] = pd.to_datetime(stock['Date'])
-  stock.sort_values('Date', ascending=True, inplace=True)
-  return stock
+
 
 vcb = get_stock('VCB')
 vnm = get_stock('VNM')
